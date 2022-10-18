98921cbbf676a83716f39fca03396a1fcce19ad7
Dawid Komęza <dawid.komeza@gmail.com>
2022-10-18 11:36:13 +0000

    Drawing field with colors as tiles

--- zadanie5/src/index.js
+++ zadanie5/src/index.js
@@ -1,6 +1,7 @@
 var Snake = /** @class */ (function () {
     function Snake(size) {
         this.field = this.createField(size);
+        this.drawField(size);
     }
     Snake.prototype.createField = function (size) {
         var field = [];
@@ -14,6 +15,31 @@ var Snake = /** @class */ (function () {
         console.table(field.length);
         return field;
     };
+    Snake.prototype.drawField = function (size) {
+        var w = window.innerWidth;
+        var cellSize = Math.floor((w / 2) / size);
+        var container = document.createElement("main");
+        var table = document.createElement("table");
+        for (var i = 0; i < size; i++) {
+            var row = document.createElement("tr");
+            for (var j = 0; j < size; j++) {
+                var cell = document.createElement("td");
+                cell.classList.add("cell");
+                cell.style.width = "".concat(cellSize, "px");
+                cell.style.height = "".concat(cellSize, "px");
+                if ((i + j) % 2 === 0) {
+                    cell.classList.add("dark");
+                }
+                else {
+                    cell.classList.add("light");
+                }
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
+        container.appendChild(table);
+        document.body.appendChild(container);
+    };
     return Snake;
 }());
-var game = new Snake(500);
+var game = new Snake(20);
--- zadanie5/src/index.ts
+++ zadanie5/src/index.ts
@@ -2,6 +2,7 @@ class Snake {
     field: number[][]
     constructor(size: number) {
         this.field = this.createField(size)
+        this.drawField(size)
     }
 
     createField(size: number) {
@@ -15,6 +16,32 @@ class Snake {
         console.table(field.length)
         return field
     }
+
+    drawField(size: number) {
+        let w = window.innerWidth
+        let cellSize = Math.floor((w / 2) / size)
+        let container = document.createElement("main")
+        let table = document.createElement("table")
+        for (let i = 0; i < size; i++) {
+            let row = document.createElement("tr")
+            for (let j = 0; j < size; j++) {
+                let cell = document.createElement("td")
+                cell.classList.add("cell")
+                cell.style.width = `${cellSize}px`
+                cell.style.height = `${cellSize}px`
+                if ((i + j) % 2 === 0) {
+                    cell.classList.add("dark")
+                }
+                else { 
+                    cell.classList.add("light")
+                }
+                row.appendChild(cell)
+            }
+            table.appendChild(row)
+        }
+        container.appendChild(table)
+        document.body.appendChild(container)
+    }
 }
 
-let game = new Snake(500)
+let game = new Snake(20)
